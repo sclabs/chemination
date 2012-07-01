386c965b693d329bea4ad44d0472ba455f71a129
Thomas Gilgenast <thomasgilgenast@gmail.com>
2012-07-01 00:36:19 -0500

    removed a method that was unintentionally added in the merge

--- org/gilgi/graph/Graph.java
+++ org/gilgi/graph/Graph.java
@@ -78,12 +78,4 @@ public class Graph<N extends Node<N>> {
 		return false;
 	}
 	
-	public int numBonds(N n) {
-		int count = 0;
-		for (N m : (List<N>) n.getNeighbors()) {
-			if (m.equals(n)) count++;
-		}
-		return count;
-	}
-	
 }
